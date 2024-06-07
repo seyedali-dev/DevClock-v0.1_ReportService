79f8be26c1fe60e72357733165bc4cb706b001f0
SayedAli <sayedxali7@gmail.com>
2024-06-07 08:46:33 +0330

    report-service initial commit

---
+++ src/main/java/com/seyed/ali/reportsservice/ReportServiceApplication.java
@@ -0,0 +1,13 @@
+package com.seyed.ali.reportsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seyed/ali/reportsservice/ReportServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seyed.ali.reportsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
